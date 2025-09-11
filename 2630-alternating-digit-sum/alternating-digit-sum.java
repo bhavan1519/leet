14b322eb1282520be13e08ebb335d2f8196bba9a
bhavan1519 <bhavanikat.24cse@kongu.edu>
2025-09-11 22:32:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (96.88%) - LeetSync

---
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int i=0,sum=0;
+        while(n>0){
+            if(i%2 == 0)
+            sum += n%10;
+            else
+            sum+=(n%10)*-1;
+            i++;
+            n/=10;
+        }
+        if(i%2 == 0) return sum*-1;
+        return sum;
+    }
+}
