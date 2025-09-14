f6b84c31e0f74f388418373cd8ba831d94b878f2
bhavan1519 <bhavanikat.24cse@kongu.edu>
2025-09-14 20:42:37 +0530

    Time: 5 ms (15.00%) | Memory: 41.4 MB (14.72%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        String s = "";
+        while(columnNumber > 0){
+            columnNumber -= 1;
+            int temp = columnNumber % 26;
+            s = (char)(65+temp)+s;
+            columnNumber /= 26;
+        }
+        return s;
+    }
+}
