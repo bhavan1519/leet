74e31ed941a2230e17bdef81a44e7ded9f66eb54
Bhavanika T <bhavanikat.24cse@kongu.edu>
2026-04-01 11:58:25 +0530

    Time: 4 ms (19.77%) | Memory: 43.3 MB (35.99%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,17 +1,22 @@
 class Solution {
     public int countGoodSubstrings(String s) {
+        HashMap<Character,Integer> hm = new HashMap<>();
         int count = 0;
-
-        for(int i = 0; i <= s.length() - 3; i++){
-            char a = s.charAt(i);
-            char b = s.charAt(i + 1);
-            char c = s.charAt(i + 2);
-
-            if(a != b && b != c && a != c){
-                count++;
+        int l=0;
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            if(i-l+1 == 3){
+                if(hm.size()==3){
+                    count++;
+                }
+                hm.put(s.charAt(l),hm.getOrDefault(s.charAt(l),0)-1);
+                if(hm.get(s.charAt(l))==0){
+                    hm.remove(s.charAt(l));
+                }
+                l++;
             }
         }
-
         return count;
     }
 }
