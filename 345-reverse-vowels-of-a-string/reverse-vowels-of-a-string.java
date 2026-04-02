71e94507c03cb28446b1d802177cb57a329ba8be
Bhavanika T <bhavanikat.24cse@kongu.edu>
2026-04-02 21:08:24 +0530

    Time: 2 ms (99.44%) | Memory: 46.6 MB (59.68%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,40 +1,28 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] a = s.toCharArray();
-        int i = 0;
-        int j = a.length - 1;
-
-        while (i < j) {
-            while (i <= a.length - 1) {
-                if (a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U') {
-                    break;
-                }
-                if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u') {
-                    break;
-                }
-                i++;
-            }
-            while (j >= 0) {
-                if (a[j] == 'A' || a[j] == 'E' || a[j] == 'I' || a[j] == 'O' || a[j] == 'U') {
-                    break;
-                }
-                if (a[j] == 'a' || a[j] == 'e' || a[j] == 'i' || a[j] == 'o' || a[j] == 'u') {
-                    break;
-                }
-                j--;
-            }
-            if (i < j) {
-                char x = a[i];
-                a[i] = a[j];
-                a[j] = x;
-                i++;
-                j--;
+        char[] arr=s.toCharArray();
+        int st=0,e=arr.length-1;
+        while(st<e){
+            if(isVowel(arr[st]) && isVowel(arr[e])){
+                char temp=arr[st];
+                arr[st]=arr[e];
+                arr[e]=temp;
+                st++;
+                e--;
+            }else if(!isVowel(arr[st])){
+                st++;
+            }else{
+                e--;
             }
         }
-        s = "";
-        for (char x : a) {
-            s = s + x;
-        }
-        return s;
+        return new String(arr);
+    }
+    public boolean isVowel(char y){
+        if(y == 'a' || y == 'e' || y == 'i' || y == 'o'
+            || y == 'u' || y == 'A' || y == 'E' || y == 'I'
+            || y == 'O' || y == 'U'){
+                return true;
+            }
+        return false;
     }
 }
