cea5f58d5751b643aa5a1b4acb35a9ad95f92b75
Bhavanika T <bhavanikat.24cse@kongu.edu>
2026-04-07 13:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (86.89%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,10 +7,10 @@ class Solution {
             return mid;
         }
         else if (nums[mid]<target){
-        left = mid+1;
+            left = mid+1;
         }
         else{ 
-        right = mid-1;
+            right = mid-1;
         }
     }
     return left;
