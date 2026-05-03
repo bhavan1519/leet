b0a42ca4e32200bfb61c46a3cab3e4a53ee54c0c
Bhavanika T <bhavanikat.24cse@kongu.edu>
2026-05-03 20:05:51 +0530

    Time: 2 ms (39.18%) | Memory: 94.6 MB (15.16%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int pro = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (buy > prices[i]) {
+                buy = prices[i];
+            }
+            pro = Math.max(pro, prices[i] - buy);
+        }
+        return pro;          
+    }
+}
