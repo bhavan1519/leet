99844dfdeaa3e38dfa8c8c984f991a9e019ff364
Bhavanika T <bhavanika1519@gmail.com>
2026-06-05 14:53:33 +0530

    Time: 10 ms (30.03%) | Memory: 52.8 MB (90.50%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,35 +1,65 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
     public int numEnclaves(int[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        boolean[][]visited = new boolean[row][col];
+
+        for(int i=0;i<col;i++){
+            if(grid[0][i]==1){
+                q.offer(new Pair(0,i));
+                visited[0][i] = true;
+            }
+            if(grid[row-1][i]==1){
+                q.offer(new Pair(row-1,i));
+                visited[row-1][i]=true;
+            }
         }
-        for(int i=0;i<grid.length;i++){
-            dfs(grid,i,0);
-            dfs(grid,i,grid[0].length-1);
+
+        for(int i=0;i<row;i++){
+            if(grid[i][0]==1){
+                q.offer(new Pair(i,0));
+                visited[i][0]=true;
+            }
+            if(grid[i][col-1]==1){
+                q.offer(new Pair(i,col-1));
+                visited[i][col-1]=true;
+            }
         }
-        for(int j=0;j<grid[0].length;j++){
-            dfs(grid,0,j);
-            dfs(grid,grid.length-1,j);
+        int[] drow = {-1,1,0,0};
+        int[] dcol = {0,0,1,-1};
+
+        while(!q.isEmpty()){
+            Pair current = q.poll();
+            int nrow = current.row;
+            int ncol = current.col;
+
+            for(int i=0;i<4;i++){
+                int ro = nrow+drow[i];
+                int co = ncol+dcol[i];
+
+                if(ro>=0 && co>=0 && ro<row && co<col && grid[ro][co]==1 && !visited[ro][co]){
+                    visited[ro][co]=true;
+                    q.offer(new Pair(ro,co));
+                }
+            }
         }
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(!visited[i][j] && grid[i][j]==1){
                     count++;
                 }
             }
         }
         return count;
-    }
-    
-    private void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
-            return;
-        }
-        grid[i][j] = -1; 
-        dfs(grid, i + 1, j); 
-        dfs(grid, i - 1, j); 
-        dfs(grid, i, j + 1); 
-        dfs(grid, i, j - 1); 
     }
 }
