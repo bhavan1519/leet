c2738ab5e46b665df3fcf3cf60496e1e6c255f7a
Bhavanika T <bhavanika1519@gmail.com>
2026-06-06 09:20:29 +0530

    Time: 1 ms (99.68%) | Memory: 47.7 MB (54.76%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+
+        if (row < 0 || col < 0 ||
+            row >= board.length || col >= board[0].length ||
+            board[row][col] != 'O') {
+            return;
+        }
+
+        board[row][col] = '#';
+
+        dfs(board, row + 1, col);
+        dfs(board, row - 1, col);
+        dfs(board, row, col + 1);
+        dfs(board, row, col - 1);
+    }
+}
