274f6172de2a6ceb157dd5ced37b9015abf5b025
Bhavanika T <bhavanika1519@gmail.com>
2026-06-06 10:17:33 +0530

    Time: 3 ms (29.39%) | Memory: 47.7 MB (54.76%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,44 +1,79 @@
+class Pair {
+    int row;
+    int col;
+
+    Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
 class Solution {
     public void solve(char[][] board) {
+
         int m = board.length;
         int n = board[0].length;
 
+        Queue<Pair> q = new LinkedList<>();
+
         for (int i = 0; i < m; i++) {
-            dfs(board, i, 0);
-            dfs(board, i, n - 1);
+
+            if (board[i][0] == 'O') {
+                q.offer(new Pair(i, 0));
+                board[i][0] = '#';
+            }
+
+            if (board[i][n - 1] == 'O') {
+                q.offer(new Pair(i, n - 1));
+                board[i][n - 1] = '#';
+            }
         }
 
         for (int j = 0; j < n; j++) {
-            dfs(board, 0, j);
-            dfs(board, m - 1, j);
+
+            if (board[0][j] == 'O') {
+                q.offer(new Pair(0, j));
+                board[0][j] = '#';
+            }
+
+            if (board[m - 1][j] == 'O') {
+                q.offer(new Pair(m - 1, j));
+                board[m - 1][j] = '#';
+            }
+        }
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+
+            Pair current = q.poll();
+            int row = current.row;
+            int col = current.col;
+
+            for (int i = 0; i < 4; i++) {
+
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && board[nrow][ncol] == 'O') {
+
+                    board[nrow][ncol] = '#';
+                    q.offer(new Pair(nrow, ncol));
+                }
+            }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                if (board[i][j] == 'O') {
+                if (board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
-                else if (board[i][j] == '#') {
+                else if (board[i][j] == '#'){
                     board[i][j] = 'O';
                 }
             }
         }
-    }
-
-    private void dfs(char[][] board, int row, int col) {
-
-        if (row < 0 || col < 0 ||
-            row >= board.length || col >= board[0].length ||
-            board[row][col] != 'O') {
-            return;
-        }
-
-        board[row][col] = '#';
-
-        dfs(board, row + 1, col);
-        dfs(board, row - 1, col);
-        dfs(board, row, col + 1);
-        dfs(board, row, col - 1);
     }
 }
