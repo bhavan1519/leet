e8e847cc9b1cef4cf44dd956c1229b4a4081dffb
Bhavanika T <bhavanika1519@gmail.com>
2026-06-09 11:45:32 +0530

    Time: 2 ms (83.18%) | Memory: 46.6 MB (63.51%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,46 +1,52 @@
 class Solution {
-    private boolean isSafePlace(int n,char[][] nQueens,int row,int col){
-        for(int i=0;i<n;i++){
-            if(nQueens[i][col] == 'Q'){
+    public List<List<String>> solveNQueens(int n) {
+        ArrayList<List<String>> al = new ArrayList<>();
+        char[][] queen = new char[n][n];
+        for(int i=0;i<queen.length;i++){
+            Arrays.fill(queen[i],'.');
+        }
+        solveUntil(queen,al,0);
+        return al;
+    }
+
+    public boolean isValid(char[][] queen,int row,int col ){
+        for(int i=col-1;i>=0;i--){
+            if(queen[row][i]=='Q'){
                 return false;
             }
         }
-        for(int i=row-1,j=col-1;i>=0 && j>= 0 ;i--,j--){
-            if(nQueens[i][j] == 'Q'){
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ; i--,j--){
+            if(queen[i][j] == 'Q'){
                 return false;
             }
         }
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(nQueens[i][j] == 'Q'){
+        for(int i=row+1,j=col-1;i<queen.length && j>=0 ; i++,j--){
+            if(queen[i][j] == 'Q'){
                 return false;
             }
         }
         return true;
     }
-    private void solveNQueens(int n, List<List<String>> output, char[][] nQueens,int row){
-        if(row == n){
-            List<String> solution = new ArrayList<>();
-            for(char[] rowArray : nQueens){
-                solution.add(new String(rowArray));
+
+    public void solveUntil(char[][] queen, ArrayList<List<String>> al,int index){
+        if(index == queen.length){
+            ArrayList<String> temp = new ArrayList<>();
+            for(int i=0;i<queen.length;i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;j<queen.length;j++){
+                    sb.append(queen[i][j]);
+                }
+                temp.add(sb.toString());
             }
-            output.add(solution);
+            al.add(temp);
             return;
         }
-        for(int col=0;col<n;col++){
-            if(isSafePlace(n,nQueens,row,col)){
-                nQueens[row][col] = 'Q';
-                solveNQueens(n,output,nQueens,row+1);
-                nQueens[row][col] = '.';
+        for(int i=0;i<queen.length;i++){
+            if(isValid(queen,i,index)){
+                queen[i][index] = 'Q';
+                solveUntil(queen,al,index+1);
+                queen[i][index]='.';
             }
         }
-    }
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> output = new ArrayList<>();
-        char[][] nQueens = new char [n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(nQueens[i],'.');
-        }
-        solveNQueens(n,output,nQueens,0);
-        return output;
     }
 }
