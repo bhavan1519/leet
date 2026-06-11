0c04d673033e66ba37f327e76f2579c3d68df7b8
Bhavanika T <bhavanika1519@gmail.com>
2026-06-11 19:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,4 +1,24 @@
+//Memorization
 class Solution{
+    public int climbStairs(int n){
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return climb(dp,n);
+    }
+    public int climb(int[] dp,int n){
+        if(n<=2){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = climb(dp,n-1)+climb(dp,n-2);
+        return dp[n];
+    }
+}
+
+//Tabulation
+/*class Solution{
     public int climbStairs(int n){
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
@@ -13,4 +33,4 @@ class Solution{
         }
         return dp[n];
     }
-}
+}*/
